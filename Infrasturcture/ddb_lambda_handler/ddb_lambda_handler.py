29c0a6c659b75406e86638c645fc6d10ede8669f
Naveen Krishna <naveenganapathyprasad@gmail.com>
2022-06-15 17:13:23 +0530

    Add destination to dynamodb workflow

---
+++ Infrasturcture/ddb_lambda_handler/ddb_lambda_handler.py
@@ -0,0 +1,24 @@
+import boto3
+
+s3 = boto3.client("s3")
+dynamodb = boto3.resource('dynamodb')
+table = dynamodb.Table('etl_final_table')
+
+def lambda_handler(event, context):
+    bucket = event["Records"][0]["s3"]["bucket"]["name"]
+    key =  event["Records"][0]["s3"]["object"]["key"]
+    response = s3.get_object(Bucket = bucket, Key= key)
+    data = response["Body"].read().decode("utf-8")
+    csvdata = data.split("\n")
+    csvdata = list(filter(None,csvdata))
+    print(csvdata)
+    for item in csvdata:
+        item_arr = item.split(",")
+        idint = int(item_arr[0])
+        table.put_item(
+            Item = {
+                "id" : idint,
+                "name" : item_arr[1],
+            })
+
+    return "done"
--- Infrasturcture/main.tf
+++ Infrasturcture/main.tf
@@ -13,15 +13,87 @@ provider "aws" {
     secret_key = var.secret_key
 }
 
-# # resource "aws_s3_bucket" "new-bucket-creation" {
-# #     bucket = "etl-pipeline-source-bucket-15062022"
-# #     tags = {
-# #         Description = "Source Bucket for our pipeline"
-# #     }
-# # }
+# resource "aws_s3_bucket" "new-bucket-creation" {
+#     bucket = "etl-pipeline-source-bucket-15062022"
+#     tags = {
+#         Description = "Source Bucket for our pipeline"
+#     }
+# }
 
-resource "aws_iam_role" "lambda_role" {
-    name = "lambda_role"
+# resource "aws_iam_role" "lambda_role" {
+#     name = "lambda_role"
+#     assume_role_policy = <<EOF
+# {
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Action" : "sts:AssumeRole",
+#         "Principal" : {
+#           "Service" : "lambda.amazonaws.com"
+#         },
+#         "Effect" : "Allow",
+#         "Sid"    : ""
+#       }
+#     ]
+# }
+#     EOF
+# }
+
+# resource "aws_iam_policy" "iam_policy_for_lambda" {
+#   name        = "aws_iam_policy_for_lambda"
+#   path        = "/"
+#   description = "AWS policy for managing aws lambda role"
+#   policy = <<EOF
+# {
+#     "Version" : "2012-10-17",
+#     "Statement" : [
+#       {
+#         "Action" : [
+#           "logs:CreateLogsGroup",
+#           "logs:CreateLogsStream",
+#           "logs:PutLogEvents"
+#         ],
+#         "Resource" : "*",
+#         "Effect"   : "Allow"
+#       }
+#     ]
+# }  
+#   EOF
+# }
+
+# resource "aws_iam_policy_attachment" "attach_policy" {
+#   name       = "lambda-role-policy-attachment"
+#   roles      = [aws_iam_role.lambda_role.name]
+#   policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+# }
+
+# data "archive_file" "zip_python_code"{
+#     type = "zip"
+#     source_file = "${path.module}/handler_code/ingest_data_to_destination_bucket.py"
+#     output_path = "${path.module}/handler_code/ingest_data_to_destination_bucket.zip"
+# }
+
+# resource "aws_lambda_function" "source_to_destination_lambda" {
+#   filename      = "${path.module}/handler_code/ingest_data_to_destination_bucket.zip"
+#   function_name = "src_to_dst_s3_lambda"
+#   role          = aws_iam_role.lambda_role.arn
+#   handler       = "ingest_data_to_destination_bucket.lambda_handler"
+#   runtime = "python3.8"
+#   depends_on = [aws_iam_policy_attachment.attach_policy]
+# }
+
+
+#-------------------- SECOND PART - DESTINATION S3 TO DYNAMO DB -------------------------
+
+resource "aws_s3_bucket" "destination-bucket-creation" {
+    bucket = "etl-pipeline-destination-bucket-15062022"
+    tags = {
+        Description = "Destination Bucket for our pipeline"
+    }
+}
+
+resource "aws_iam_role" "ddb_lambda_role" {
+    name = "ddb_lambda_role"
     assume_role_policy = <<EOF
 {
     "Version" : "2012-10-17",
@@ -39,45 +111,68 @@ resource "aws_iam_role" "lambda_role" {
     EOF
 }
 
-resource "aws_iam_policy" "iam_policy_for_lambda" {
-  name        = "aws_iam_policy_for_lambda"
+resource "aws_iam_policy" "iam_policy_for_ddb_lambda" {
+  name        = "aws_iam_policy_ddb_for_lambda"
   path        = "/"
-  description = "AWS policy for managing aws lambda role"
+  description = "AWS policy for managing aws ddb lambda role"
   policy = <<EOF
 {
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Action" : [
-          "logs:CreateLogsGroup",
-          "logs:CreateLogsStream",
-          "logs:PutLogEvents"
-        ],
-        "Resource" : "*",
-        "Effect"   : "Allow"
-      }
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": "*",
+            "Resource": "*"
+        }
     ]
 }  
   EOF
 }
 
-resource "aws_iam_policy_attachment" "attach_policy" {
-  name       = "lambda-role-policy-attachment"
-  roles      = [aws_iam_role.lambda_role.name]
-  policy_arn = aws_iam_policy.iam_policy_for_lambda.arn
+resource "aws_iam_policy_attachment" "attach_policy_ddb_lambda" {
+  name       = "ddb_lambda-role-policy-attachment"
+  roles      = [aws_iam_role.ddb_lambda_role.name]
+  policy_arn = aws_iam_policy.iam_policy_for_ddb_lambda.arn
 }
 
-data "archive_file" "zip_python_code"{
+data "archive_file" "zip_ddb_python_code"{
     type = "zip"
-    source_file = "${path.module}/handler_code/ingest_data_to_destination_bucket.py"
-    output_path = "${path.module}/handler_code/ingest_data_to_destination_bucket.zip"
+    source_file = "${path.module}/ddb_lambda_handler/ddb_lambda_handler.py"
+    output_path = "${path.module}/ddb_lambda_handler/ddb_lambda_handler.zip"
 }
 
-resource "aws_lambda_function" "source_to_destination_lambda" {
-  filename      = "${path.module}/handler_code/ingest_data_to_destination_bucket.zip"
-  function_name = "src_to_dst_s3_lambda"
-  role          = aws_iam_role.lambda_role.arn
-  handler       = "ingest_data_to_destination_bucket.lambda_handler"
+resource "aws_lambda_function" "destination_bucket_to_ddb_lambda" {
+  filename      = "${path.module}/ddb_lambda_handler/ddb_lambda_handler.zip"
+  function_name = "dst_s3_to_ddb_lambda"
+  role          = aws_iam_role.ddb_lambda_role.arn
+  handler       = "ddb_lambda_handler.lambda_handler"
   runtime = "python3.8"
-  depends_on = [aws_iam_policy_attachment.attach_policy]
+  depends_on = [aws_iam_policy_attachment.attach_policy_ddb_lambda]
+}
+
+resource "aws_s3_bucket_notification" "ddb_lambda_trigger" {
+  bucket = aws_s3_bucket.destination-bucket-creation.id
+  lambda_function {
+    lambda_function_arn = aws_lambda_function.destination_bucket_to_ddb_lambda.arn
+    events              = ["s3:ObjectCreated:*", "s3:ObjectRemoved:*"]
+
+  }
+}
+
+resource "aws_dynamodb_table" "etl_final_table" {
+    name = "etl_final_table"
+    hash_key = "id"
+    billing_mode = "PAY_PER_REQUEST"
+    attribute {
+      name = "id"
+      type = "N"
+    }
+}
+
+resource "aws_lambda_permission" "test" {
+  statement_id  = "AllowS3Invoke"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.destination_bucket_to_ddb_lambda.function_name
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${aws_s3_bucket.destination-bucket-creation.id}"
 }
